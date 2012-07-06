13c033ae11ff041e0ac24e6ffb0713b9cf25445a
Jeff McKenzie <mail@mcknz.com>
2012-07-06 15:22:04 -0400

    ui core prototype ramblings

--- scripts/ui.js
+++ scripts/ui.js
@@ -1,49 +1,47 @@
 /*global c3 */
 
-c3.ui = {};
-
 c3.ui = (function(app, board, game) {
   "use strict";
   var $;
-  function getSquare(squareId){}
-  function getSquareContent(squareId){}
-  function setSquareContent(squareId, content){}
-  function getBoardPiece(piece){}
-  function setBoardSquare(squareId, pieceSize, playerId) {
-    board.setSquarePiece(squareId, app.piece(pieceSize, playerId), game);
-  }
-  function paintSquare(squareId, piece) {
-    set
-    var square = document.getElementById("s" + squareId),
-            content;
 
-        if (app.isNone(newPiece.size)) {
-          content = "&nbsp;";
-        } else {
-          content = '<img src="images/piece' + newPiece.size + '_player' + newPiece.playerId + '.svg" />';
-        }
-        $.setInnerHTML(square, content);
+  function Core($) {
+    this.app = app;
+    this.board = board;
+    this.game = game;
   }
-  function disableSquare(squareId) {}
-  function disableBoard() {}
-  function setMessage(msg) {}
-  function changePlayerTurn(playerId) {}
-  function hitSquare(squareId) {}
-  function setup() {}
 
-  function init(obj) {
-    var copy = new obj.constructor(),
-        attr;
-    for (attr in ) {
-      if (obj.hasOwnProperty(attr)) {
-        this[attr] = obj[attr];
-      }
-            }
-            return copy;
+  Core.prototype.getSquare = function(squareId){};
+  Core.prototype.getSquareContent = function(squareId){};
+  Core.prototype.setSquareContent = function(squareId, content){};
+  Core.prototype.getBoardPiece = function(piece){};
+  Core.prototype.setBoardSquare = function(squareId, pieceSize, playerId) {
+      board.setSquarePiece(squareId, app.piece(pieceSize, playerId), game);
+  };
+  Core.prototype.paintSquare function paintSquare(squareId, piece) {
+      set
+      var square = document.getElementById("s" + squareId),
+              content;
+
+          if (app.isNone(newPiece.size)) {
+            content = "&nbsp;";
+          } else {
+            content = '<img src="images/piece' + newPiece.size + '_player' + newPiece.playerId + '.svg" />';
+          }
+          $.setInnerHTML(square, content);
+    }
+    function disableSquare(squareId) {}
+    function disableBoard() {}
+    function setMessage(msg) {}
+    function changePlayerTurn(playerId) {}
+    function hitSquare(squareId) {}
+    function setup() {}
+  };
+
+  function init(lib, sub) {
+    var ui = new core()
+    $ = lib;
+
   }
-    return {
-        type:"square",
-        playerId:playerId,
-        size:size
-    };
+
 }(c3, c3.board, c3.game));
+
